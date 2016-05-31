8f4e23a431894a37a27a502a9e17c189b733e9e7
Nilay Jayswal <nilayjayswalnj@gmail.com>
2016-05-31 18:44:52 +0530

    1,2 Upload

---
+++ Multiply/src/Multiply/Multiply.java
@@ -0,0 +1,13 @@
+package Multiply;
+
+public class Multiply {
+	public static void main(String args[]) {
+		int num;
+		num=100;
+		System.out.println("This is num " + num);
+		num = num*2;
+		System.out.println("num *2 is " + num);
+		
+	}
+
+}
